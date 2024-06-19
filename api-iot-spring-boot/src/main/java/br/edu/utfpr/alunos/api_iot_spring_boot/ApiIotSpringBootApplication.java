d49ec9c95a7423ac70bd441c688366aaff65dbc0
Carlos Moyses <carlos.moyses@gmail.com>
2024-06-18 23:06:20 -0300

    projeto inicial docker TAC

---
+++ api-iot-spring-boot/src/main/java/br/edu/utfpr/alunos/api_iot_spring_boot/ApiIotSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.alunos.api_iot_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIotSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIotSpringBootApplication.class, args);
+	}
+
+}
---
+++ api-iot-spring-boot/src/test/java/br/edu/utfpr/alunos/api_iot_spring_boot/ApiIotSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.alunos.api_iot_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIotSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
